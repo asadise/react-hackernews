35d78562136eb9b67bb30d2de57c38cab4e22874
Morteza <info@asadiweb.ir>
2020-05-16 19:18:55 +0430

    using ref in DOM element

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+    componentDidMount() {
+        if (this.input) {
+            this.input.focus();
+        }
+    }
+
     render() {
         const { value, onSubmit, onChange, children } = this.props;
         return (
             <div>
                 <form onSubmit={onSubmit}>
-                    <input type="text" placeholder={children} value={value} onChange={onChange} />
+                    <input type="text" placeholder={children} value={value} onChange={onChange} ref={(node) => { this.input = node; }} />
                     &nbsp;<button type="submit">{children}</button>
                 </form>
                 <br /><br /><br />
